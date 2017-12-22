014b6ddbc16c936b39c07337dc4f9af58319cc78
bw4sz <benweinstein2010@gmail.com>
2017-12-22 10:44:12 -0800

    google env

--- Cloud/Job.sh
+++ Cloud/Job.sh
@@ -1,9 +1,16 @@
 #!/bin/bash 
 
-docker run -it gcr.io/api-project-773889352370/rwhales bin/bash
+#get git keys
+gsutil cp -r gs://api-project-773889352370-ml/.ssh/id_rsa ~/.ssh/
+gsutil cp -r gs://api-project-773889352370-ml/.ssh/id_rsa.pub ~/.ssh/
+
+chmod 400 ~/.ssh/id_rsa
+
+#run docker container and pass keys
+sudo docker run -it -v /home/ben/.ssh/:/root/.ssh/ gcr.io/api-project-773889352370/rwhales bin/bash 
 
 #clone
-git clone https://github.com/bw4sz/WhalePhys.git --depth 1
+git clone git@github.com:bw4sz/WhalePhys.git --depth 1
 
 cd WhalePhys
 
@@ -14,9 +21,18 @@ git checkout -b $(hostname)
 Rscript -e "rmarkdown::render('RunModel.Rmd')" &> run.txt
 
 #push results
+git config --global user.email "benweinstein2010@gmail.com"
+git config --global user.name "bw4sz"
+
 git add --all
 git commit -m "Cloud run complete"
-git push -u origin $iid
+git push -u origin $(hostname)
 
-#kill instance
-sudo halt
+#kill container
+exit
+
+#kill host
+exit
+
+#delete host
+gcloud compute instances delete -q cloudml
--- Cloud/Startup.sh
+++ Cloud/Startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash     
     
 #name it the instance ID
-gcloud beta compute instances create-with-container instance-1 --container-image=gcr.io/api-project-773889352370/rwhales 
-      
+gcloud compute instances create cloudml \
+    --image-family=container-vm \
+    --image-project=google-containers \
+    --machine-type=n1-highmem-4
  #--metadata-from-file startup-script=Job.sh
