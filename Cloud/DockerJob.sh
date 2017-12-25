3a616b922bad0c4d45cdd35423a72d8cb5f2beeb
bw4sz <benweinstein2010@gmail.com>
2017-12-25 14:27:44 -0800

    change in docker job run format

---
+++ Cloud/DockerJob.sh
@@ -0,0 +1,22 @@
+#!/bin/bash     
+
+#add git to known hosts?
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+#clonex
+git clone git@github.com:bw4sz/WhalePhys.git --depth 1
+cd WhalePhys
+
+#make new branch      
+git checkout -b $(hostname)
+
+#render script
+Rscript -e "rmarkdown::render('RunModel.Rmd')" &> run.txt
+
+#push results
+git config --global user.email "benweinstein2010@gmail.com"
+git config --global user.name "bw4sz"
+
+git add --all
+git commit -m "Cloud run complete"
+git push -u origin $(hostname)
--- Cloud/Job.sh
+++ Cloud/Job.sh
@@ -6,36 +6,11 @@ gsutil cp -r gs://api-project-773889352370-ml/.ssh/id_rsa.pub ~/.ssh/
 
 chmod 400 ~/.ssh/id_rsa
 
+#get file to run
+gsutil cp -r gs://api-project-773889352370-ml/.ssh/id_rsa.pub ~/.ssh/
+
 #run docker container and pass keys
-sudo docker run -it -v /home/ben/.ssh/:/root/.ssh/ gcr.io/api-project-773889352370/rwhales bin/bash 
-
-#add git to known hosts?
-ssh-keyscan github.com >> ~/.ssh/known_hosts
-
-#clonex
-git clone git@github.com:bw4sz/WhalePhys.git --depth 1
-
-cd WhalePhys
-
-#make new branch      
-git checkout -b $(hostname)
-
-#render script
-Rscript -e "rmarkdown::render('RunModel.Rmd')" &> run.txt
-
-#push results
-git config --global user.email "benweinstein2010@gmail.com"
-git config --global user.name "bw4sz"
-
-git add --all
-git commit -m "Cloud run complete"
-git push -u origin $(hostname)
-
-#kill container
-exit
-
-#kill host
-exit
+sudo docker run -v /home/ben/.ssh/:/root/.ssh/ gcr.io/api-project-773889352370/rwhales DockerJob.sh
 
 #delete host
 gcloud compute instances delete -q cloudml
